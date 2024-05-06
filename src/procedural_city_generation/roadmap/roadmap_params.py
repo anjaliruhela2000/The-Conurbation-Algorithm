294fbb71423dcf6631fc203295696e28e4c72354
Anjali Ruhela <anjali_ruhela@yahoo.in>
2024-05-06 15:34:20 +0530

    Update roadmap_params.py

--- src/procedural_city_generation/roadmap/roadmap_params.py
+++ src/procedural_city_generation/roadmap/roadmap_params.py
@@ -1,13 +1,3 @@
-"""
-Module which contains all input parameters for this submodule, along with:
-- The default (recommmended) value from the developers
-- A short description of what the parameter changes
-- All values that the parameter accepts without causing this program to break.
-
-This information can also be viewed when calling the GUI and clicking the "options"
-button for that specific module.
-
-"""
 from procedural_city_generation.additional_stuff.Param import Param
 params = [
     Param("axiom", [[0, 0], [1, 0], [0, -2], [0, -3]], [[0, 0], [1, 0], [0, -2], [0, -3]],
