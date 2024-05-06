d166d1d45c4aefeb727f6c519b82355705eabe1d
Anjali Ruhela <anjali_ruhela@yahoo.in>
2024-05-06 15:31:09 +0530

    Update input_image_setup.py

--- src/procedural_city_generation/roadmap/config_functions/input_image_setup.py
+++ src/procedural_city_generation/roadmap/config_functions/input_image_setup.py
@@ -1,24 +1,4 @@
 def input_image_setup(rule_image_name, density_image_name):
-    '''
-    Loads the rule-image and population-density-image from the filesystem.
-    Saves the density image in /temp/ folder so that it could be ensured.
-
-    Parameters
-    ----------
-    rule_image_name: String
-        Name of the rule_image specified in Parameters
-    density_image_name: String
-        Name of the density_image specified in Parameters
-
-    Returns
-    --------
-    rule_img: np.ndarray
-        Rule-image as numpy array
-    density_img: np.ndarray
-        Density-image as numpy array
-    '''
-
-    #TODO: Document
     import matplotlib.image as mpimg
     import procedural_city_generation
     import os
