26649fb723f00e935bf60de1a338b62181e731a2
Anjali Ruhela <anjali_ruhela@yahoo.in>
2024-05-06 15:17:39 +0530

    Update rotate.py

--- src/procedural_city_generation/additional_stuff/rotate.py
+++ src/procedural_city_generation/additional_stuff/rotate.py
@@ -1,16 +1,6 @@
 import numpy as np
 
 def rotate(angle, vector):
-    """
-    Rotates a 2D Vector.
-
-    Parameters
-    ----------
-    angle : float
-        Angle in which the Vector is rotated in mathematically-positive direction
-    vector : np.array(2, )
-        Vector to be rotated
-    """
     #Explicit case because it gets called very often
     if angle == 90:
         return np.array([-vector[1], vector[0]])
