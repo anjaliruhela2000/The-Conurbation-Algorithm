461afc8f9da763256f1f241cfcd5ceff61eb27ed
Anjali Ruhela <anjali_ruhela@yahoo.in>
2024-05-06 15:19:33 +0530

    Update building_generation_params.py

--- src/procedural_city_generation/building_generation/building_generation_params.py
+++ src/procedural_city_generation/building_generation/building_generation_params.py
@@ -1,13 +1,3 @@
-"""
-Module which contains all input parameters for this sudmodule, along with:
-- The default (recommmended) value from the developers
-- A short description of what the parameter changes
-- All values that the parameter accepts without causing this program to break.
-
-This information can also be viewed when calling the GUI and clicking the "options"
-button for that specific module.
-
-"""
 from procedural_city_generation.additional_stuff.Param import Param
 
 params = [Param("house_height", 0.15, 0.15, "The buildingheight which determines whether a building is a house or an appartment/skyscraper", "float"),
